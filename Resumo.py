a48a8c0f83736d11b34cf7cbea82283cd5caf96b
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-10-22 19:33:08 -0300

    SOBRESCRITA

--- Resumo.py
+++ Resumo.py
@@ -48,62 +48,6 @@ print(p1.crescer())
 print(p2.crescer())
 print(p2.crescer())
 
-'''
-Agora vamos falar sobre o conceito que representa a ideia de
-generalização/especialização em POO que é a HERANÇA
-'''
-#com a herança podemos criar uma classe mais genérica e espessificar melhor em outras classes 
-#chamamos a classe mais genérica de classe Mãe ou Superclasse
-#já as classes especícas chamamos de filha ou subclasses e elas herdam atributos e métodos da classe Mãe
-
-#EXEMPLO:
-#IMPLEMENTANDO A SUPERCLASSE 
-class Animal:
-    def __init__(self, nome, cor):
-        self.nome = nome
-        self.cor = cor
-        
-    def comer(self):
-        print(f'o {self.nome} está comendo...')
-    
-#IMPLEMENTANDO AS SUBCLASSES
-class Gato(Animal):
-    def __init__(self, nome, cor, vidas):
-        super().__init__(nome, cor)
-        self.vidas = vidas
-    
-    def PerdeuVida(self):
-        self.vidas = self.vidas -1
-        return 'agora o '+ self.nome +' tem ' + str(self.vidas) + ' vidas.'
-
-class Coelho(Animal):
-    def __init__(self, nome, cor):
-        super().__init__(nome, cor)
-    
-    def Pular(self):
-        print(f'o {self.nome} está pulando')
-
-#iniciando a aplicação das classes 
-a = Animal('Gus', 'preto')
-g = Gato('Mingau', 'Branco', 7)
-c = Coelho('squik', 'marrom')
-
-a.comer()
-g.comer()
-c.comer()
-
-print(g.PerdeuVida())
-c.Pular()
-
-#em pytohon também temos um método que diz se um objeto é uma instancia da classe, por exemplo:
-print(isinstance(a, Animal))
-print(isinstance(g, Animal))
-print(isinstance(c, Animal))
-print(isinstance(g, Gato))
-print(isinstance(g, Coelho))
-print(isinstance(c, Gato))
-print(isinstance(c, Coelho))
-
 '''MÉTODOS MÁGICOS'''
 #Utilizados para que definir um comportamento específico para uma classe quando determinada operação acontecer
 #Geralmente, não são executados pelos mesmos nomes que são utilizados para defini-los
@@ -165,7 +109,204 @@ produto = matriz1 * matriz2
 print("Produto das matrizes:")
 print(produto)
 
+#outra coisa que podemos fazer é representar a classe como um dicionário, para isso usamos os seguintes comamdos:
+print(vars(matriz1))
+print(matriz1.__dict__)
+
 '''AGORA VAMOS FALAS SOBRE OS 4 PILARES DA POO'''
+
+'''Abstração'''
+#Este conceito nos permite 'focar' apenas no que nos importa para o código
+#por exemplo vamos criar uma classe pessoa, uma pessoa te infinitos atributos e métodos no mundo real mas para o mundo computacional escolhemos apenas o que é do nosso interesse
+#uma pessoa pode ter cor, esporte, senha, tipo sanguineo, cpf
+#mas para a classe pessoa usaremos apenas senha e cpf
+
+'''Encapsulamento'''
+#Encapsulamento é um princípio relacionado a POO que nos orienta a esconder as funcionalidades e funcionamento do nosso ocódigo dentro de pequenas unidades (normalmente métodos).
+'''
+• Python NÃO TEM modificadores de acesso
+• Convenções:
+• public: Pode ser usado em qualquer lugar (atributo sem underline)
+• protected: Deve ser usado apenas na classe e subclasses _ (um underline)
+• private: DEVE ser usado apenas na classe em que foi declarado
+__ (dois underlines)'''
+#Exemplo:
+class Comodo:
+    def __init__(self, metragem):
+        self.__metragem = metragem # o '__' indica que o método é privado e só pode ser lido na classe com os metodos getters e setters como fizemos a baixo
+
+    @property
+    def metragem(self):
+        return self.__metragem
+
+    @metragem.setter
+    def metragem (self, metragem):
+        self.__metragem= metragem
+
+quarto=Comodo("7m2")
+print(quarto.metragem)
+quarto.metragem="10m2"
+print(quarto.metragem)
+
+'''Herança'''
+#com a herança podemos criar uma classe mais genérica e espessificar melhor em outras classes 
+#chamamos a classe mais genérica de classe Mãe ou Superclasse
+#já as classes especícas chamamos de filha ou subclasses e elas herdam atributos e métodos da classe Mãe
+
+#EXEMPLO:
+#IMPLEMENTANDO A SUPERCLASSE 
+class Animal:
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+        
+    def comer(self):
+        print(f'o {self.nome} está comendo...')
+    
+#IMPLEMENTANDO AS SUBCLASSES
+class Gato(Animal):
+    def __init__(self, nome, cor, vidas):
+        super().__init__(nome, cor)
+        self.vidas = vidas
+    
+    def PerdeuVida(self):
+        self.vidas = self.vidas -1
+        return 'agora o '+ self.nome +' tem ' + str(self.vidas) + ' vidas.'
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    
+    def Pular(self):
+        print(f'o {self.nome} está pulando')
+
+#iniciando a aplicação das classes 
+a = Animal('Gus', 'preto')
+g = Gato('Mingau', 'Branco', 7)
+c = Coelho('squik', 'marrom')
+
+a.comer()
+g.comer()
+c.comer()
+
+print(g.PerdeuVida())
+c.Pular()
+
+#em pytohon também temos um método que diz se um objeto é uma instancia da classe, por exemplo:
+print(isinstance(a, Animal))
+print(isinstance(g, Animal))
+print(isinstance(c, Animal))
+print(isinstance(g, Gato))
+print(isinstance(g, Coelho))
+print(isinstance(c, Gato))
+print(isinstance(c, Coelho))
+
+'''Polimorfismo'''
+#Polimorfismo é a característica única de linguagens orientadas a objetos que permite que diferentes objetos respondam a mesma mensagem cada um a sua maneira.
+#Exemplo:
+class Vaca:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def emitirSom(self):
+        print(f'o {self.nome} está mugindo')
+
+class Cachorro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def emitirSom(self):
+        print(f'o {self.nome} está latindo')
+
+v = Vaca('maribel')
+c = Cachorro('luke')
+
+v.emitirSom()
+c.emitirSom()
+
+
+'''
+SOBRESCRITA
+• Sobrescrita é a possibilita de rescrever métodos com o mesmo nome em classes filhas.
+• Sobrescrita existe quando há relação de herança entre as classes.
+• Sobrescrita permite especializar os métodos herdados das superclasses, alterando o seu comportamento nas subclasses por um comportamento mais específico.
+'''
+#EXEMPLO:
+
+class Integrante_IFRN:
+    def __init__(self, matricula, senha):
+        self.__matricula = matricula
+        self.__senha = senha
+
+    @property
+    def matricula(self):
+        return self.__matricula
+
+    @matricula.setter
+    def matricula(self, matricula):
+        self.__matricula = matricula
+    
+    @property
+    def senha(self):
+        return self.__senha
+
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
+
+    def exibirMensagem(self):
+        print('Seja bem vindo(a) ao IFRN!!!')
+
+class Professor(Integrante_IFRN):
+    def __init__(self, matricula, senha, materia):
+        super().__init__(matricula, senha)
+        self.__materia = materia
+
+    @property
+    def materia(self):
+        return self.__materia
+
+    @materia.setter
+    def materia(self, materia):
+        self.__materia = materia
+    
+    def exibirMensagem(self):
+        print('Meus alunos são os melhores!!!')
+
+class Aluno(Integrante_IFRN):
+    def __init__(self, matricula, senha, boletin):
+        super().__init__(matricula, senha)
+        self.__boletin = boletin
+
+    @property
+    def boletin(self):
+        return self.__boletin
+
+    @boletin.setter
+    def boletin(self, boletin):
+        self.__boletin = boletin
+
+    def exibirMensagem(self):
+        print('Vou estudar pra tirar 100 em POO!!!')
+
+a = Integrante_IFRN('20221174010009', 'borboletinha')
+b = Professor('20221174010010', 'glimpse', 'biologia')
+c = Aluno('20221174010012', 'paoComMortadela', 'boletin.csv')
+
+print(a.matricula)
+print(a.senha)
+a.exibirMensagem()
+
+print(b.matricula)
+print(b.senha)
+print(b.materia)
+b.exibirMensagem()
+
+print(c.matricula)
+print(c.senha)
+print(c.boletin)
+c.exibirMensagem()
+
 
 
 
