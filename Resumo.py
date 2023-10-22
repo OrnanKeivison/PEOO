a77ef72ec0949b74010e3026ac790c689dc7ec36
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-10-22 19:03:52 -0300

    métodos mágicos

--- Resumo.py
+++ Resumo.py
@@ -95,5 +95,78 @@ c.comer()
 print(g.PerdeuVida())
 c.Pular()
 
+#em pytohon também temos um método que diz se um objeto é uma instancia da classe, por exemplo:
+print(isinstance(a, Animal))
+print(isinstance(g, Animal))
+print(isinstance(c, Animal))
+print(isinstance(g, Gato))
+print(isinstance(g, Coelho))
+print(isinstance(c, Gato))
+print(isinstance(c, Coelho))
+
+'''MÉTODOS MÁGICOS'''
+#Utilizados para que definir um comportamento específico para uma classe quando determinada operação acontecer
+#Geralmente, não são executados pelos mesmos nomes que são utilizados para defini-los
+
+#TOMANDO COMO EXEMPLO UMA MATRIZ 
+#não podemos fazer soma ou multiplicação usando * ou +, mas se usarmos os métodos especiais...
+
+class MatrizQuadrada:
+    def __init__(self, a11, a12, a21, a22):
+        self.a11 = a11
+        self.a12 = a12
+        self.a21 = a21
+        self.a22 = a22
+
+        self.Matriz = [[self.a11, self.a12], [self.a21, self.a22]]
+
+    def __str__(self):
+        return f'| {self.a11}  {self.a12} |\n| {self.a21}  {self.a22} |'
+
+    def __add__(self, other):
+        if isinstance(other, MatrizQuadrada):
+            result = MatrizQuadrada(
+                self.a11 + other.a11,
+                self.a12 + other.a12,
+                self.a21 + other.a21,
+                self.a22 + other.a22
+            )
+            return result
+        else:
+            raise ValueError("Você só pode somar duas matrizes quadradas.")
+
+    def __mul__(self, other):
+        if isinstance(other, MatrizQuadrada):
+            result = MatrizQuadrada(
+                self.a11 * other.a11 + self.a12 * other.a21,
+                self.a11 * other.a12 + self.a12 * other.a22,
+                self.a21 * other.a11 + self.a22 * other.a21,
+                self.a21 * other.a12 + self.a22 * other.a22
+            )
+            return result
+        else:
+            raise ValueError("Você só pode multiplicar duas matrizes quadradas.")
+
+# Exemplo de uso
+matriz1 = MatrizQuadrada(1, 2, 3, 4)
+matriz2 = MatrizQuadrada(5, 6, 7, 8)
+
+print("Matriz 1:")
+print(matriz1)
+
+print("Matriz 2:")
+print(matriz2)
+
+soma = matriz1 + matriz2
+print("Soma das matrizes:")
+print(soma)
+
+produto = matriz1 * matriz2
+print("Produto das matrizes:")
+print(produto)
+
+'''AGORA VAMOS FALAS SOBRE OS 4 PILARES DA POO'''
+
+
 
 
